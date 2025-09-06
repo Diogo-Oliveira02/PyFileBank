f847bafd344ffd1d8fcac6c5fa0b4972235a5093
Diogo-Oliveira02 <diogo.oss29@gmail.com>
2025-09-06 15:29:34 -0300

    Corrigir erro, criando função para reescrever CSV

--- app.py
+++ app.py
@@ -3,11 +3,32 @@ import csv                    # Implementa classes para ler e gravar dados tabul
 import os                     # Interagir com o sistema operaciona, executa comando no terminal
 
 
-# Esse código lê o arquivo CSV, transforma cada linha em um dicionário e cria um grande dicionário clientes
-with open('cadastro.csv', newline='', encoding="utf-8-sig") as FileCSV:
-    leitor_arquivo = csv.DictReader(FileCSV, delimiter=";")
-    clientes = {}
-    for linha in leitor_arquivo:
-        chave = linha["nome"]
-        clientes[chave] = linha
-        print(clientes)
+# Lê o arquivo CSV, transforma cada linha em um dicionário e cria um grande dicionário clientes
+try:
+    with open('cadastro.csv', newline='', encoding="utf-8-sig") as FileCSV:
+        leitor_arquivo = csv.DictReader(FileCSV, delimiter=";")
+        clientes = {}
+        for linha in leitor_arquivo:
+            chave = linha["nome"]
+            clientes[chave] = linha
+except FileNotFoundError:
+    print("Arquvo csv não encontrado!")
+    
+# Salva todos os clientes no arquivo CSV, sobrescrevendo os dados antigos
+def alterar_saldo_csv():
+    with open('cadastro.csv', 'w', newline='', encoding='utf-8-sig') as FileCSV:
+        escrever_arquivo = csv.DictWriter(FileCSV, fieldnames=clientes[chave],delimiter=";")
+        escrever_arquivo.writeheader()
+        for cliente in clientes.values():
+            escrever_arquivo.writerow(cliente)
+
+def exibir_menu():
+    print("""
+        ========== Banco Digital ==========
+        [1] - Deposito dinheiro
+        [2] - Sacar dinheiro
+        [3] - Trasferir dinheiro para outra conta
+        [4] - Extrato de conta
+        [5] - Consulta Saldo
+        [0] - Sair
+    """)
